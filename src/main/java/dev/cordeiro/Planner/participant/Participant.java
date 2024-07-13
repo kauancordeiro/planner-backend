fc4aa3ca3971672d6d5003d514bbff1da4fb0929
Kauan <cvitorcordeiro@gmail.com>
2024-07-13 16:07:08 -0300

    participant entity

---
+++ src/main/java/dev/cordeiro/Planner/participant/Participant.java
@@ -0,0 +1,36 @@
+package dev.cordeiro.Planner.participant;
+
+import dev.cordeiro.Planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "participant")
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+}
