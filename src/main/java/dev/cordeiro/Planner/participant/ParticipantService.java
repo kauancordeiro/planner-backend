7ba579ca162fc12742de4469d39f06196ba7b5cb
Kauan <cvitorcordeiro@gmail.com>
2024-07-13 13:50:06 -0300

    migrations

---
+++ src/main/java/dev/cordeiro/Planner/participant/ParticipantService.java
@@ -0,0 +1,18 @@
+package dev.cordeiro.Planner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+
+
+    public void registerParticipantToEvent(List<String> participantsToInvite, UUID tripId){
+
+    }
+    public void triggerConfirmationEmailToParticipants(UUID tripId){
+
+    }
+}
