0bfa277aa53f948c600fd5d7cb072bc31af18844
Kauan <cvitorcordeiro@gmail.com>
2024-07-14 12:01:27 -0300

    create entity activity

---
+++ src/main/java/dev/cordeiro/Planner/activities/Activity.java
@@ -0,0 +1,34 @@
+package dev.cordeiro.Planner.activities;
+
+import dev.cordeiro.Planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "activities")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+
+
+}
